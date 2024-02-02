fd3d8351505291bb630bc283dd5f3fb65f3adf3c
Princy Patel <kapatelprincy@gmail.com>
2024-02-03 00:06:36 +0530

    update gitignore file

---
+++ src/Data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        id: "1",
+        name: "Princy",
+        email: "princy@gmail.com"
+    },
+    {
+        id: "2",
+        name: "Anjali",
+        email: "anjali@gmail.com"
+    },
+    {
+        id: "3",
+        name: "Yatra",
+        email: "yatra@gmail.com"
+    },
+]
+
+export default data;
